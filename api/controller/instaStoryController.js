ef4ebb61d4d8aa2f8ed6dbf7b1e1eb4cce5874a6
Digvijay Singh <Digvijaysinghonly1@gmail.com>
2024-05-16 09:53:32 +0530

    api changes

--- api/controller/instaStoryController.js
+++ api/controller/instaStoryController.js
@@ -1,8 +1,17 @@
 const InstaStory = require("../models/instaStoryModel");
 
-const getInstaStory = async (query) => {
+const getInstaStory = async (params) => {
   try {
-    const res = await InstaStory.find({});
+    let query = {};
+    let find = {};
+    if (params?.userId) {
+      query._id = params?.userId;
+    }
+
+    if (params?.banner) {
+      find = { _id: 1, user: 1, profilePic: 1 };
+    }
+    const res = await InstaStory.find(query, find);
     return res;
   } catch (err) {
     console.error(err, "Error");
--- api/routes/instaStoryRoutes.js
+++ api/routes/instaStoryRoutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const instaStoryController = require("../controller/instaStoryController");
 
-router.get("/get", async (req, res) => {
+router.get("/stories", async (req, res) => {
   try {
-    const result = await instaStoryController.getInstaStory();
+    const result = await instaStoryController.getInstaStory({ ...req?.query });
     res.status(200).json(result);
   } catch (error) {
     res.status(500).json({ message: error.message });
